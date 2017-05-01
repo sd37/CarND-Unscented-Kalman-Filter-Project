a1b760f8989f3c6922244e4ab4bd8117556e1482
spandan <spandan@prosperworks.com>
2017-05-01 00:12:49 -0700

    Prediction done

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -82,12 +82,6 @@ UKF::UKF() {
 
     Xsig_pred_ = MatrixXd(n_x_, n_aug_);
 
-
-    // initial state vector
-    x_ = VectorXd(5);
-
-    // initial covariance matrix
-    P_ = MatrixXd(5, 5);
     P_ << 1, 0, 0, 0, 0,
             0, 1, 0, 0, 0,
             0, 0, 1, 0, 0,
@@ -145,7 +139,7 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
 
             x_ << px, py, v, 0, 0;
 
-        } else if(meas_package.sensor_type_ == MeasurementPackage::LASER) {
+        } else if (meas_package.sensor_type_ == MeasurementPackage::LASER) {
             // intialize state
             x_ << meas_package.raw_measurements_[0], meas_package.raw_measurements_[1], 0.0, 0.0, 0.0;
         }
@@ -165,14 +159,14 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
     Prediction(dt);
 
     if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
-        if(!use_radar_) {
+        if (!use_radar_) {
             return;
         }
         UpdateRadar(meas_package);
 
     } else {
-       // Laser Updates
-        if(!use_laser_) {
+        // Laser Updates
+        if (!use_laser_) {
             return;
         }
         UpdateLidar(meas_package);
@@ -191,6 +185,109 @@ void UKF::Prediction(double delta_t) {
     Complete this function! Estimate the object's location. Modify the state
     vector, x_. Predict sigma points, the state, and the state covariance matrix.
     */
+
+    // Generate Sigma Points
+    //
+
+    //create augmented mean vector
+    VectorXd x_aug = VectorXd(n_aug_);
+
+    //create augmented state covariance
+    MatrixXd P_aug = MatrixXd(n_aug_, n_aug_);
+
+    //create sigma point matrix
+    MatrixXd Xsig_aug = MatrixXd(n_aug_, total_sigma_points_);
+
+    //create augmented mean state
+    x_aug.head(5) = x_;
+    x_aug(5) = 0;
+    x_aug(6) = 0;
+
+
+    //create augmented covariance matrix
+    P_aug.fill(0.0);
+    P_aug.topLeftCorner(5, 5) = P_;
+    P_aug(5, 5) = std_a_ * std_a_;
+    P_aug(6, 6) = std_yawdd_ * std_yawdd_;
+
+    //create square root matrix
+    MatrixXd L = P_aug.llt().matrixL();
+
+    //create augmented sigma points
+    Xsig_aug.col(0) = x_aug;
+    for (int i = 0; i < n_aug_; i++) {
+        Xsig_aug.col(i + 1) = x_aug + sqrt(lambda_ + n_aug_) * L.col(i);
+        Xsig_aug.col(i + 1 + n_aug_) = x_aug - sqrt(lambda_ + n_aug_) * L.col(i);
+    }
+
+    // Sigma Point prediction.
+    // Just pass in through the process model.
+    //
+
+    for (int i = 0; i < 2 * n_aug_ + 1; i++) {
+        //extract values for better readability
+        double p_x = Xsig_aug(0, i);
+        double p_y = Xsig_aug(1, i);
+        double v = Xsig_aug(2, i);
+        double yaw = Xsig_aug(3, i);
+        double yawd = Xsig_aug(4, i);
+        double nu_a = Xsig_aug(5, i);
+        double nu_yawdd = Xsig_aug(6, i);
+
+        //predicted state values
+        double px_p, py_p;
+
+        //avoid division by zero
+        if (fabs(yawd) > 0.001) {
+            px_p = p_x + v / yawd * (sin(yaw + yawd * delta_t) - sin(yaw));
+            py_p = p_y + v / yawd * (cos(yaw) - cos(yaw + yawd * delta_t));
+        } else {
+            px_p = p_x + v * delta_t * cos(yaw);
+            py_p = p_y + v * delta_t * sin(yaw);
+        }
+
+        double v_p = v;
+        double yaw_p = yaw + yawd * delta_t;
+        double yawd_p = yawd;
+
+        //add noise
+        px_p = px_p + 0.5 * nu_a * delta_t * delta_t * cos(yaw);
+        py_p = py_p + 0.5 * nu_a * delta_t * delta_t * sin(yaw);
+        v_p = v_p + nu_a * delta_t;
+
+        yaw_p = yaw_p + 0.5 * nu_yawdd * delta_t * delta_t;
+        yawd_p = yawd_p + nu_yawdd * delta_t;
+
+        //write predicted sigma point into right column
+
+        Xsig_pred_(0, i) = px_p;
+        Xsig_pred_(1, i) = py_p;
+        Xsig_pred_(2, i) = v_p;
+        Xsig_pred_(3, i) = yaw_p;
+        Xsig_pred_(4, i) = yawd_p;
+    }
+
+    // mean and covariance estimation from predicted sigma points
+    //
+
+    //predicted state mean
+    x_.fill(0.0);
+    for (int i = 0; i < total_sigma_points_; i++) {  //iterate over sigma points
+        x_ = x_+ weights_(i) * Xsig_pred_.col(i);
+    }
+
+    //predicted state covariance matrix
+    P_.fill(0.0);
+    for (int i = 0; i < total_sigma_points_; i++) {  //iterate over sigma points
+
+        // state difference
+        VectorXd x_diff = Xsig_pred_.col(i) - x_;
+        //angle normalization
+        while (x_diff(3)> M_PI) x_diff(3)-=2.*M_PI;
+        while (x_diff(3)<-M_PI) x_diff(3)+=2.*M_PI;
+
+        P_ = P_ + weights_(i) * x_diff * x_diff.transpose() ;
+    }
 }
 
 /**
