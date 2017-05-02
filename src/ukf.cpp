f4ab48173ec1b145a497d6330cf6515f4a2ff458
spandan <spandan@prosperworks.com>
2017-05-01 21:01:49 -0700

    comments added

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -28,10 +28,10 @@ UKF::UKF() {
     P_ = MatrixXd(5, 5);
 
     // Process noise standard deviation longitudinal acceleration in m/s^2
-    std_a_ = 2.0;
+    std_a_ = 2.0; // The acceleration seems to be between -4 to 4 m/s from data
 
     // Process noise standard deviation yaw acceleration in rad/s^2
-    std_yawdd_ = 0.5;
+    std_yawdd_ = 0.5; // The acceleration seems to be -1 to 1 m/s from data.
 
     // Laser measurement noise standard deviation position1 in m
     std_laspx_ = 0.15;
